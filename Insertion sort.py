d7aceed1d042cdcf6ce88d9877053b470123ff43
Sandhyarani <smudindi34219@ucumberlands.edu>
2025-01-09 18:26:38 -0600

    Added code for Insertion sort

---
+++ Insertion sort.py
@@ -0,0 +1,17 @@
+def insertion_sort_desc_order(arr):
+    for i in range(1,len(arr)):
+        key = arr[i]
+        position = i
+        while position > 0 and arr[position - 1] < key :
+            arr[position] = arr[position - 1]
+            position = position - 1
+            arr[position] = key
+
+
+
+array = [56,78,90,2,1,67,88, 99] 
+print('original array:',array)  
+insertion_sort_desc_order(array)
+print("sorted array in desending order:",array)
+
+
