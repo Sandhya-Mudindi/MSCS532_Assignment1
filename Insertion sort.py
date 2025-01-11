3981046bac777dc9b05d191b6ca4f93ada6de212
Sandhyarani <smudindi34219@ucumberlands.edu>
2025-01-11 11:41:12 -0600

    Added comments

--- Insertion sort.py
+++ Insertion sort.py
@@ -1,11 +1,15 @@
 def insertion_sort_desc_order(arr):
-    for i in range(1,len(arr)):
-        key = arr[i]
-        position = i
-        while position > 0 and arr[position - 1] < key :
+    for i in range(len(arr)):
+        key = arr[i]  # select the current element
+        position = i  #  start from first element
+
+        # shift the largest element sorted part to the right side
+        while position > 0 and arr[position - 1] < key :  
             arr[position] = arr[position - 1]
             position = position - 1
-            arr[position] = key
+            
+            # place the element at the correct position
+            arr[position] = key  
 
 if __name__ == "__main__":
     array = [56,78,90,2,1,67,88, 99]
